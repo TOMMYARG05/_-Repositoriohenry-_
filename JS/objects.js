f325b5445cbaba1f67e5926906dfa6b05c7f26a3
Tomas <tomasmolinarc@gmail.com>
2024-07-30 16:13:48 -0300

    Actualizacion 30/07/2024

---
+++ JS/objects.js
@@ -0,0 +1,26 @@
+/*var Deportes = {
+    conPelota:['Football','Barkeyball','Rugby'],
+    sinPelota:['Trekking','Montainbike','Skii'],
+
+};*/
+var persona = {nombre: 'Tomas', edad: 18, estudios: {tecnico_en_programacion:true}};
+
+
+persona.nombre = 'Tomas Mateo';
+persona.edad = 19;
+console.log(persona.edad,persona.nombre);
+
+
+var autos={};
+autos.marcas=['Ford','Dodge','Nissan'];
+console.log(autos);
+
+
+
+var misFunciones={
+    saludar: function ()  {
+        console.log("Hola");
+    },
+};
+
+misFunciones.saludar();
